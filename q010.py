75d079e3c26bef17c30d104422139163bef3d384
SAHIL RAWAT <sahilrawat1415@gmail.com>
2026-07-17 16:40:07 +0530

    Delete q010.py

--- q010.py
+++
@@ -1,7 +0,0 @@
-sec=int(input("Enter no. of seconds you want to convert :"))
-min=sec//60
-remaining_sec=sec%60
-hour=min//60
-remaining_min=min%60
-print(min,"minutes",remaining_sec,"second")
-print(hour,'Hours',remaining_min,"min")
